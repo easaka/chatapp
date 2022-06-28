338f0d99499a623174df8088c65aa85ec283f347
easaka <sakakwame2002@gmail.com>
2022-06-28 16:08:22 +0000

    Displays user and rooms

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :rooms
+  root 'pages#home'
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get 'users', to: 'devise/session#new'
+  end
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get 'user/:id', to: 'users#show', as: 'User'
+end
